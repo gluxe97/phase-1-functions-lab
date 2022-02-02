41913355c3dafad4d7b17b11d65dab00070d291b
gluxe97 <96441367+gluxe97@users.noreply.github.com>
2022-02-02 13:32:10 -0600

    commit tree 2a5548bcab5c20fdfdbab16593ef6c288d6e3011

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(customerloc){
+    return Math.abs(42-customerloc);
+}
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks)*264;
+}
+function distanceTravelledInFeet(start, stop) {
+    const travel = (stop-start);
+    return distanceFromHqInFeet(distanceFromHqInBlocks(travel));
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination)>2500) {
+        return "cannot travel that far";
+    }
+    if (distanceTravelledInFeet(start, destination)>2000) {
+        return 25;
+    }
+    if (distanceTravelledInFeet(start, destination)>400) {
+        return (distanceTravelledInFeet(start, destination)-400) * .02;
+    }
+    else {
+        return 0
+    }
+}
